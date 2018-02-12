632347f432025c9db33a253ee4856efbdc7f26b2
Torben Jaeger <torben@jit-central.com>
2018-02-12 16:39:37 +0100

    fixed NPE

--- src/main/java/com/j1t/MyTransformerBean.java
+++ src/main/java/com/j1t/MyTransformerBean.java
@@ -1,12 +1,15 @@
 package com.j1t;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.component.file.GenericFile;
 import org.apache.camel.component.telegram.model.OutgoingPhotoMessage;
 
 public class MyTransformerBean {
 
     public void transform(Exchange exchange) {
         OutgoingPhotoMessage msg = new OutgoingPhotoMessage();
+        msg.setCaption(exchange.getIn().getBody(GenericFile.class).getFileNameOnly());
+        msg.setFilenameWithExtension(msg.getCaption());
         msg.setPhoto(exchange.getIn().getBody(byte[].class));
         exchange.getIn().setBody(msg);
     }
